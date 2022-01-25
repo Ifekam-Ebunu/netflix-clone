be2d835bbc060d0839f39a63208172960e93fbab
Ifekam Ebunu <daveifekam95@gmail.com>
2022-01-25 20:53:39 +0100

    full netflix clone website built with html, css and javasript

---
+++ app.js
@@ -0,0 +1,60 @@
+// QUESTION 1
+const answer1 = document.getElementById("answer1")
+const question1 = document.getElementById("question1")
+question1.addEventListener("click", function(){
+    if (answer1.style.display !== "none"){
+        answer1.style.display = "none"
+    }else{
+        answer1.style.display = "block"
+    }
+})
+// QUESTION 2
+const answer2 = document.getElementById("answer2")
+const question2 = document.getElementById("question2")
+question2.addEventListener("click", function(){
+    if (answer2.style.display !== "none"){
+        answer2.style.display = "none"
+    }else{
+        answer2.style.display = "block"
+    }
+})
+// QUESTION 3
+const answer3 = document.getElementById("answer3")
+const question3 = document.getElementById("question3")
+question3.addEventListener("click", function(){
+    if (answer3.style.display !== "none"){
+        answer3.style.display = "none"
+    }else{
+        answer3.style.display = "block"
+    }
+})
+// QUESTION 4
+const answer4 = document.getElementById("answer4")
+const question4 = document.getElementById("question4")
+question4.addEventListener("click", function(){
+    if (answer4.style.display !== "none"){
+        answer4.style.display = "none"
+    }else{
+        answer4.style.display = "block"
+    }
+})
+// QUESTION 5
+const answer5 = document.getElementById("answer5")
+const question5 = document.getElementById("question5")
+question5.addEventListener("click", function(){
+    if (answer5.style.display !== "none"){
+        answer5.style.display = "none"
+    }else{
+        answer5.style.display = "block"
+    }
+})
+// QUESTION 6
+const answer6 = document.getElementById("answer6")
+const question6 = document.getElementById("question6")
+question6.addEventListener("click", function(){
+    if (answer6.style.display !== "none"){
+        answer6.style.display = "none"
+    }else{
+        answer6.style.display = "block"
+    }
+})
